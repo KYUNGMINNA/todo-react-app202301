5c54ac74668995c3d7cdddfd6248c3fe14a9d7e3
KYUNGMINNA <nkm4095@naver.com>
2023-01-20 15:48:13 +0900

    # 초기 설정

---
+++ README.md
@@ -0,0 +1,6 @@
+# React app 만들기
+```
+
+$ npx create-react-app 프로젝트명  or $ yarn create react-app todo-app
+$ npm start : ract 실행
+```
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>안녕안녕</h1> 
+    //HTML이 아니고 JS임 그래서 class 대신 className
+    //label for 대신 htmlFor 이런식으로 사용
+  );
+}
+
+export default App;

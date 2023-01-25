f12265910bab97b4a7bbba062f3444c10bad074c
KYUNGMINNA <nkm4095@naver.com>
2023-01-25 14:54:13 +0900

    # css 변경

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/TodoInput.css';
 import {MdAdd} from 'react-icons/md'
+import cn from 'classnames';
+//별칭을 cn이라고 지정
+
+
 const TodoInput = () => {
+
+  // todo-input 박스를 렌더링할지 여부
+  const [open, setOpen] = useState(false);
+
+  // todo-input 박스를 열고 닫는 클릭이벤트 핸들러
+  const inputToggle = () => setOpen(!open);
+
+
+
+
+
   return (
 
     <>
+
+    {/* open이 true면 밑의 태그들이 렌더링, 아니면 렌더링 안함 */}
+    {open &&
     <div className='todo-input'>
        {/* 웹 표준에 의하면  input은 form태그 안에 사용(form 을 안쓰더라도
         ) */}
@@ -15,7 +33,11 @@ const TodoInput = () => {
             autoFocus />
        </form>
     </div>
-    <button className='begin-btn'>
+    }
+
+                    {/*클래스이름 , 상태변수(t,f) -->t면 
+                    클래시이름 open추가 */}
+    <button className={cn('begin-btn',{open})} onClick={inputToggle}>
       <MdAdd />
       </button>
 

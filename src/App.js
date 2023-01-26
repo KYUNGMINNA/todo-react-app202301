dbcc5d62da73b2e31c2886607a98ee4802a43625
KYUNGMINNA <nkm4095@naver.com>
2023-01-26 15:58:12 +0900

    # 회원 가입 입력값 검증

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import ItemMain from './components/item/ItemMain';
 import TodoTemplate from './components/todo/TodoTemplate';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Join from './components/user/Join';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -21,7 +23,8 @@ function App() {
       <FoodList /> */}
 
       <Header />
-      <TodoTemplate />
+      {/* <TodoTemplate /> */}
+      <Join />
       <Footer />
 
     </>
